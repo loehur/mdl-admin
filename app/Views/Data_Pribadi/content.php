ca9c6754e919a8092d0d69f3164a4003c76050cb
loehur <loehur@gmail.com>
2023-07-20 16:34:19 +0700

    upload ktp2

--- app/Views/Data_Pribadi/content.php
+++ app/Views/Data_Pribadi/content.php
@@ -8,7 +8,7 @@
 
 <!-- Main page content-->
 <div class="row ms-1">
-    <div class="col-auto bg-white">
+    <div class="col bg-white">
         <div class="row">
             <div class="col py-1 line100"><small><u>Status</u></small><br>
                 <?php
@@ -23,108 +23,99 @@
                 } ?>
             </div>
         </div>
-        <div class="row">
-            <div class="col py-1 line100 text-success"><small><u>KTP:</u></small><br>
-                <img style="max-width: 300px;" src="<?= $this->BASE_URL . $this->userData['ktp_path'] ?>" class="img-fluid" alt="...">
-            </div>
+        <div class="col py-1 line100 text-success"><small><u>KTP:</u></small><br>
+            <img style="max-width: 270px;" src="<?= $this->BASE_URL . $this->userData['ktp_path'] ?>" class="img-fluid" alt="...">
         </div>
+
         <div class="row">
-            <div class="col py-1 line100 text-success"><small><u>NIK:</u></small><br><b><?= $this->userData['nik'] ?></b></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>Nama</u></small><br><?= $this->userData['nama'] ?></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>No. Handphone</u></small><br><?= $this->userData['hp'] ?></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>Penghasilan/Bulan</u></small><br>Rp<?= number_format($this->userData['penghasilan']) ?></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>Alamat</u></small><br><?= strtoupper($this->userData['alamat']) ?></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>Provinsi</u></small><br><?= strtoupper($this->userData['provinsi']) ?></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>Kota/Kabupaten</u></small><br><?= strtoupper($this->userData['kota']) ?></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>Kecamatan</u></small><br><?= strtoupper($this->userData['kecamatan']) ?></div>
-        </div>
-        <div class="row">
-            <div class="col py-1 line100"><small><u>Kelurahan</u></small><br><?= strtoupper($this->userData['kelurahan']) ?></div>
+            <div class="col-md-6 py-1 line100 text-success"><small><u>NIK:</u></small><br><b><?= $this->userData['nik'] ?></b></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>Nama</u></small><br><?= $this->userData['nama'] ?></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>No. Handphone</u></small><br><?= $this->userData['hp'] ?></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>Penghasilan/Bulan</u></small><br>Rp<?= number_format($this->userData['penghasilan']) ?></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>Alamat</u></small><br><?= strtoupper($this->userData['alamat']) ?></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>Provinsi</u></small><br><?= strtoupper($this->userData['provinsi']) ?></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>Kota/Kabupaten</u></small><br><?= strtoupper($this->userData['kota']) ?></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>Kecamatan</u></small><br><?= strtoupper($this->userData['kecamatan']) ?></div>
+
+            <div class="col-md-6 py-1 line100"><small><u>Kelurahan</u></small><br><?= strtoupper($this->userData['kelurahan']) ?></div>
+
         </div>
     </div>
-</div>
 
-<form action="<?= $this->BASE_URL . $data['_c'] ?>/ktp" class="upload" method="POST">
-    <div class="modal" id="modal_ktp">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Update Data KTP</h5>
-                </div>
-                <div class="modal-body">
-                    <div class="container">
-                        <div class="row mb-2">
-                            <div class="col">
-                                <label class="form-label">NIK</label>
-                                <input type="text" class="form-control" name="nik">
+    <form action="<?= $this->BASE_URL . $data['_c'] ?>/ktp" class="upload" method="POST">
+        <div class="modal" id="modal_ktp">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Update Data KTP</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container">
+                            <div class="row mb-2">
+                                <div class="col">
+                                    <label class="form-label">NIK</label>
+                                    <input type="text" class="form-control" name="nik">
+                                </div>
+                                <div class="col">
+                                    <label for="formFileSm" class="form-label">KTP (<span class="text-danger">Max. 10mb</span>)</label>
+                                    <input class="form-control form-control-sm" type="file" id="file" name="ktp" required /> [ <span id="persen"><b>0</b></span><b> %</b> ] Upload Progress
+                                </div>
                             </div>
-                            <div class="col">
-                                <label for="formFileSm" class="form-label">KTP (<span class="text-danger">Max. 10mb</span>)</label>
-                                <input class="form-control form-control-sm" type="file" id="file" name="ktp" required /> [ <span id="persen"><b>0</b></span><b> %</b> ] Upload Progress
-                            </div>
-                        </div>
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <button type="submit" data-bs-dismiss="modal" class="btn btn-primary">Simpan</button>
+                            <div class="row mb-2">
+                                <div class="col-sm-6">
+                                    <button type="submit" data-bs-dismiss="modal" class="btn btn-primary">Simpan</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</form>
+    </form>
 
-<script src="<?= $this->ASSETS_URL ?>js/jquery-3.7.0.min.js"></script>
+    <script src="<?= $this->ASSETS_URL ?>js/jquery-3.7.0.min.js"></script>
 
-<script>
-    $("form.upload").on("submit", function(e) {
-        e.preventDefault();
-        var formData = new FormData(this);
-        var file = $('#file')[0].files[0];
-        formData.append('file', file);
+    <script>
+        $("form.upload").on("submit", function(e) {
+            e.preventDefault();
+            var formData = new FormData(this);
+            var file = $('#file')[0].files[0];
+            formData.append('file', file);
 
-        $.ajax({
-            xhr: function() {
-                var xhr = new window.XMLHttpRequest();
-                xhr.upload.addEventListener("progress", function(evt) {
-                    if (evt.lengthComputable) {
-                        var percentComplete = (evt.loaded / evt.total) * 100;
-                        $('#persen').html('<b>' + Math.round(percentComplete) + '</b>');
+            $.ajax({
+                xhr: function() {
+                    var xhr = new window.XMLHttpRequest();
+                    xhr.upload.addEventListener("progress", function(evt) {
+                        if (evt.lengthComputable) {
+                            var percentComplete = (evt.loaded / evt.total) * 100;
+                            $('#persen').html('<b>' + Math.round(percentComplete) + '</b>');
+                        }
+                    }, false);
+                    return xhr;
+                },
+                url: $(this).attr('action'),
+                type: 'POST',
+                data: formData,
+                contentType: "application/octet-stream",
+                enctype: 'multipart/form-data',
+
+                contentType: false,
+                processData: false,
+
+                success: function(res) {
+                    if (res == 0) {
+                        content();
+                    } else {
+                        alert(res);
                     }
-                }, false);
-                return xhr;
-            },
-            url: $(this).attr('action'),
-            type: 'POST',
-            data: formData,
-            contentType: "application/octet-stream",
-            enctype: 'multipart/form-data',
-
-            contentType: false,
-            processData: false,
-
-            success: function(res) {
-                if (res == 0) {
-                    content();
-                } else {
-                    alert(res);
-                }
-            },
+                },
+            });
         });
-    });
-</script>
+    </script>
