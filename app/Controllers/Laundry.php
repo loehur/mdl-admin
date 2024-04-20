3bebbd61887e46c9cb7c393b5fa431e59431ca9c
loehur <loehur@gmail.com>
2024-04-20 15:55:48 +0700

    transfer cabang laundry

--- app/Controllers/Laundry.php
+++ app/Controllers/Laundry.php
@@ -7,20 +7,18 @@ class Laundry extends Controller
       //laundry target
       $set_laundry = "id_laundry = " . $laundry_target;
 
-      //penjualan
-      $where = "id_cabang = " . $cabang . " AND tuntas = 0";
-      $penjualan = $this->db(1)->get_where("penjualan", $where);
-
       //pindahkan cabang ke laundry
       $this->db(1)->update("cabang", $set_laundry, 'id_cabang = ' . $cabang);
       $this->db(1)->update("user", $set_laundry, 'id_cabang = ' . $cabang);
       $this->db(1)->update("operasi", $set_laundry, 'id_cabang = ' . $cabang);
+      $this->db(1)->update("penjualan", $set_laundry, 'id_cabang = ' . $cabang);
+   }
 
-      foreach ($penjualan as $p) {
-         $where_p = "id_penjualan = " . $p['id_penjualan'];
-
-         //transfer penjualan ke laundry
-         $this->db(1)->update("penjualan", $set_laundry, $where_p);
-      }
+   function transfer_member($cabang, $id_harga_sumber, $id_harga_target)
+   {
+      $set = "id_harga = " . $id_harga_target;
+      $where = "id_harga = " . $id_harga_sumber . " AND id_cabang = " . $cabang;
+      $this->db(1)->update("penjualan", $set, $where);
+      $this->db(1)->update("member", $set, $where);
    }
 }
